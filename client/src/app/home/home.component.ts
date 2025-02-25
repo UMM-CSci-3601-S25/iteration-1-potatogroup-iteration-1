345ace4246a200f2b59ed13da64fda926eacb0e6
Dylan Packer <packe089@morris.umn.edu>
2025-02-25 13:28:01 -0600

    Updated the home page so that the "Rules and Information" button is displayed in a way so that easy to find and nice to look at

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { PopupComponent } from '../InformationPage/info.component';
 
 @Component({
   selector: 'app-home-component',
   templateUrl: 'home.component.html',
   styleUrls: ['./home.component.scss'],
   providers: [],
-  imports: [MatCardModule]
+  imports: [MatCardModule, MatDialogModule, MatButtonModule, MatIconModule, MatTooltipModule]
 })
 export class HomeComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openDialog() {
+    this.dialog.open(PopupComponent);
+  }
 }
