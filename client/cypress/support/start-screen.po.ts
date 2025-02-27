8851c7e55c3962c85d48b8ff3c84b3b04a29e50b
Anton Olson <ols00559@morris.umn.edu>
2025-02-27 13:34:33 -0600

    Added E2E tests for the home page and its elements
    In this commit, I added 5 tests that test the various buttons, their redirections, and the ability to see various components (e.g., the title of each page).

---
+++ client/cypress/support/start-screen.po.ts
@@ -0,0 +1,44 @@
+export class StartScreenPage {
+
+  private readonly url = '';
+  private readonly title = '.start-screen';
+  private readonly joinButton = '.join-button';
+  private readonly hostButton = '.host-button';
+  private readonly snackBar = '.mat-mdc-simple-snack-bar';
+  private readonly formFieldSelector = 'mat-form-field';
+  private readonly dropDownSelector = 'mat-option';
+
+  navigateTo() {
+    return cy.visit(this.url);
+  }
+
+  getTitle() {
+    return cy.get(this.title);
+  }
+
+  getJoinButton() {
+    return cy.get(this.joinButton);
+  }
+  getHostButton() {
+    return cy.get(this.hostButton);
+  }
+
+  selectMatSelectValue(select: Cypress.Chainable, value: string) {
+    // Find and click the drop down
+    return select.click()
+      // Select and click the desired value from the resulting menu
+      .get(`${this.dropDownSelector}[value="${value}"]`).click();
+  }
+
+  getFormField(fieldName: string) {
+    return cy.get(`${this.formFieldSelector} [formcontrolname=${fieldName}]`);
+  }
+
+  getSnackBar() {
+    // Since snackBars are often shown in response to errors,
+    // we'll add a timeout of 10 seconds to help increase the likelihood that
+    // the snackbar becomes visible before we might fail because it
+    // hasn't (yet) appeared.
+    return cy.get(this.snackBar, { timeout: 10000 });
+  }
+}
