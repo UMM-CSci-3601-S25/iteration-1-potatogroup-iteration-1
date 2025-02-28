15c1fd5d76ec75224a77fcc6b84d7b196b8ce87c
Dylan Packer <packe089@morris.umn.edu>
2025-02-28 12:14:03 -0600

    Added the rules button to the host and join screens so that they are there if the players want.

--- client/src/app/host/host.component.ts
+++ client/src/app/host/host.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Router} from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { PopupComponent } from '../InfoPage/info.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-host-component',
   templateUrl: 'host.component.html',
   styleUrls: ['./host.component.scss'],
-  imports: [MatCardModule]
+  imports: [MatCardModule, MatIconModule]
 })
 
-
 export class HostComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, public dialog: MatDialog) {}
 
+  openDialog() {
+    this.dialog.open(PopupComponent);
+  }
 }
