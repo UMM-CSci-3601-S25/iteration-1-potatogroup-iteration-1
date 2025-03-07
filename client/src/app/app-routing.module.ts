eae907e2e7ba4cac4dc7316ec884a404dec871cd
Dylan Packer <packe089@morris.umn.edu>
2025-03-07 12:17:14 -0600

    This should be what a player needs to add new players to the game

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AddUserComponent } from './users/add-user.component';
+//import { AddUserComponent } from './users/add-user.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { CompanyListComponent } from './company-list/company-list.component';
 import { JoinComponent } from './join/join.component';
 import { HostComponent } from './host/host.component';
+import { judgeComponent } from './judge/judge.component';
+//import { LobbyComponent } from './lobby/lobby.component'; // Ensure the path is correct and the file exists
 
 // Note that the 'users/new' route needs to come before 'users/:id'.
 // If 'users/:id' came first, it would accidentally catch requests to
@@ -14,11 +16,13 @@ import { HostComponent } from './host/host.component';
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
   {path: 'users', component: UserListComponent, title: 'Users'},
-  {path: 'users/new', component: AddUserComponent, title: 'Add User'},
+  //{path: 'users/new', component: AddUserComponent, title: 'Add User'},
   {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'},
   {path: 'companies', component: CompanyListComponent, title: 'Companies'},
   {path: 'join', component: JoinComponent, title: 'Join'},
-  {path: 'host', component: HostComponent, title: 'Host'}
+  {path: 'host', component: HostComponent, title: 'Host'},
+  {path: 'judge', component: judgeComponent, title: 'Judge'},
+  //{path: 'lobby', component: LobbyComponent, title: 'Lobby'} // Add route for LobbyComponent
 ];
 
 @NgModule({
--- client/src/app/join/join.component.ts
+++ client/src/app/join/join.component.ts
@@ -1,21 +1,164 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { Router} from '@angular/router';
-import { MatDialog } from '@angular/material/dialog';
-import { PopupComponent } from '../InfoPage/info.component';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { UserRole } from '../users/user';
+import { UserService } from '../users/user.service';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-join-component',
-  templateUrl: 'join.component.html',
+  selector: 'app-join',
+  templateUrl: './join.component.html',
   styleUrls: ['./join.component.scss'],
-  imports: [MatCardModule, MatIconModule]
+  imports: [FormsModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatButtonModule, MatIconModule]
 })
-
 export class JoinComponent {
-  constructor(private router: Router, public dialog: MatDialog) {}
+
+  joinForm = new FormGroup({
+    name: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+      (fc) => {
+        if (fc.value.toLowerCase() === 'abc123' || fc.value.toLowerCase() === '123abc') {
+          return ({existingName: true});
+        } else {
+          return null;
+        }
+      },
+    ])),
+
+    age: new FormControl<number>(null, Validators.compose([
+      Validators.required,
+      Validators.min(15),
+      Validators.max(200),
+      Validators.pattern('^[0-9]+$')
+    ])),
+
+    company: new FormControl(''),
+
+    email: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.email,
+    ])),
+
+    role: new FormControl<UserRole>('player' as UserRole, Validators.compose([
+      Validators.required,
+      Validators.pattern('^(admin|editor|viewer)$'),
+    ])),
+
+    card1: new FormControl('', Validators.required),
+    card2: new FormControl('', Validators.required),
+    card3: new FormControl('', Validators.required),
+    card4: new FormControl('', Validators.required),
+    card5: new FormControl('', Validators.required),
+  });
+
+  readonly joinValidationMessages = {
+    name: [
+      {type: 'required', message: 'Name is required'},
+      {type: 'minlength', message: 'Name must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Name cannot be more than 50 characters long'},
+      {type: 'existingName', message: 'Name has already been taken'}
+    ],
+
+    age: [
+      {type: 'required', message: 'Age is required'},
+      {type: 'min', message: 'Age must be at least 15'},
+      {type: 'max', message: 'Age may not be greater than 200'},
+      {type: 'pattern', message: 'Age must be a whole number'}
+    ],
+
+    email: [
+      {type: 'email', message: 'Email must be formatted properly'},
+      {type: 'required', message: 'Email is required'}
+    ],
+
+    role: [
+      { type: 'required', message: 'Role is required' },
+      { type: 'pattern', message: 'Role must be Admin, Editor, or Viewer' },
+    ]
+  };
+
+  constructor(
+    private userService: UserService,
+    private snackBar: MatSnackBar,
+    private router: Router) {
+  }
+
+  formControlHasError(controlName: string): boolean {
+    return this.joinForm.get(controlName).invalid &&
+      (this.joinForm.get(controlName).dirty || this.joinForm.get(controlName).touched);
+  }
+
+  getErrorMessage(name: keyof typeof this.joinValidationMessages): string {
+    for(const {type, message} of this.joinValidationMessages[name]) {
+      if (this.joinForm.get(name).hasError(type)) {
+        return message;
+      }
+    }
+    return 'Unknown error';
+  }
 
   openDialog() {
-    this.dialog.open(PopupComponent);
+    // Logic to open a dialog with rules and information
+    console.log('Dialog opened');
   }
+
+  submitForm() {
+    const formValue = this.joinForm.value;
+    const user = {
+      name: formValue.name,
+      age: formValue.age,
+      company: formValue.company,
+      email: formValue.email,
+      role: formValue.role,
+      cards: [
+        formValue.card1,
+        formValue.card2,
+        formValue.card3,
+        formValue.card4,
+        formValue.card5
+      ]
+    };
+
+    this.userService.addUser(user).subscribe({
+      next: () => {
+        this.snackBar.open(
+          `Joined as user ${formValue.name}`,
+          null,
+          { duration: 2000 }
+        );
+        this.router.navigate(['/lobby']); // Navigate to lobby page
+      },
+      error: err => {
+        if (err.status === 400) {
+          this.snackBar.open(
+            `Tried to join with an illegal user – Error Code: ${err.status}\nMessage: ${err.message}`,
+            'OK',
+            { duration: 5000 }
+          );
+        } else if (err.status === 500) {
+          this.snackBar.open(
+            `The server failed to process your request to join. Is the server up? – Error Code: ${err.status}\nMessage: ${err.message}`,
+            'OK',
+            { duration: 5000 }
+          );
+        } else {
+          this.snackBar.open(
+            `An unexpected error occurred – Error Code: ${err.status}\nMessage: ${err.message}`,
+            'OK',
+            { duration: 5000 }
+          );
+        }
+      },
+    });
+  }
+
 }
